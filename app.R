2ad59cc11c8ffdb534190c99d8f3c832518c2f16
mringland <mringland@ucsb.edu>
2021-02-23 22:50:20 -0800

    updated home page. all tabs have content now but none of the graphs are working properly :(

--- app.R
+++ app.R
@@ -4,13 +4,20 @@
 library(shiny)
 library(tidyverse)
 library(bslib)
+library(here)
+library(sf) # for spatial data
+library(tmap) # for interactive maps
+library(leaflet)
+library(tmaptools)
+library(mapview)
 
+# Set themes
 dark_theme <- bs_theme(
-  bg = "#26428B",
+  bg = "#053d57",
   fg = "#FFFAF0",
   primary = "#E8CCD7",
   base_font = font_google("Roboto"),
-  heading_font = font_google("Cinzel"))
+  heading_font = font_google("Arvo"))
 
 light_theme <- bs_theme(
   bg = "#FFFAF0",
@@ -26,84 +33,182 @@ white_back_theme <- bs_theme(
   base_font = font_google("Roboto"),
   heading_font = font_google("Alegreya Sans SC"))
 
+
+# User Interface
+
 ui <- fluidPage(theme = dark_theme,
                 
                 navbarPage("Carbon Counters: Evaluating the Climate Mitigation Potential of Santa Barbara County's Natural and Working Lands",
                            tabPanel("Home",
                                     titlePanel(
-                                               "Welcome to Our Shiny App - this will change"),
-                                    mainPanel(align = "center",
-                                              "Acknowledging that these lands can play a significant role in reducing emissions, the County of Santa Barbara is now adding a Natural and Working Lands component to the 2022 update of its Climate Action Plan. Our team’s role in addressing this problem is to quantify the carbon storage potential of these lands, and help integrate that information into county planning for increased carbon storage into the future.
-",
-                                              h3("as always a photo of farm stuff"),
-                                              img(src = "farms1.jpg", height = 400, width = 600), #image not working
-                                              
+                                      "Evaluating the Climate Mitigation Potential of Santa Barbara County's Natural and Working Lands"),
+                                    mainPanel(align = "left", br(),
+                                              "Acknowledging the significant role that natural and working lands (NWL) can play in reducing greenhouse gas emissions, the County of Santa Barbara is adding a NWL component to the 2022 update of its Climate Action Plan.",
+                                              br(),
+                                              br(),
+                                              "Our team’s role in addressing this problem is to quantify the carbon storage potential of these lands, and help integrate that information into county planning for increased carbon storage into the future.",
+                                              br(),
+                                              br(),
+                                              img(src = "farms1.jpg", height = 400, width = 700),
+                                              br(),
+                                              "add citation for photo",
+                                              br(),
+                                              br(),
                                               h2("Project Objectives"),
-                                              "1. Calculate a Countywide carbon inventory by accounting for carbon (stock) storage and emissions associated with Santa Barbara County’s natural and working lands.\n 2. Project land use change and resulting carbon stock and emissions, using a baseline trend from historical data. \n 3. Engage the agricultural community to ensure our modeling and recommendations are based in reality. \n 4. Assess the changes to forecasted stock and emissions from different land management scenarios. \n  5. Recommend realistic greenhouse gas reduction and management strategies to the County.",
-                                              
-                                    h2("Timeline Maybe"),
-                                    "State targets of reaching 40% below 1990 emissions levels by 2030, and reaching carbon neutrality by 2045. Santa Barbara County has set an even more ambitious target (or just scaled?) for 2030, to reduce emissions 50% below 2007 levels.",
-                                    h3("and maybe a map of the county"),
+                                              "1. Calculate a Countywide carbon inventory by accounting for carbon (stock) storage and emissions associated with Santa Barbara County’s natural and working lands.",
+                                              br(),
+                                              "2. Project land use change and resulting carbon stock and emissions to 2030, using a baseline trend from historical data.",
+                                              br(),
+                                              "3. Engage the agricultural community to ensure our modeling and recommendations are based in reality.",
+                                              br(),
+                                              "4. Assess the changes to forecasted stock and emissions from different land management scenarios.",
+                                              br(),
+                                              "5. Recommend realistic greenhouse gas reduction and management strategies to the County.",
+                                              br(),
+                                              br(),
+                                              h2("Climate Goals: Show Timeline Maybe"),
+                                              "State targets of reaching 40% below 1990 emissions levels by 2030, and reaching carbon neutrality by 2045. Santa Barbara County has set an equivalent target for 2030, to reduce emissions 50% below 2007 levels.",
+                                              br(),
+                                              br(),
+                                              img(src = "CountyMap.gif", height = 500, width = 700),
                                     )),
                            
+                           # First Tab
                            tabPanel("Carbon Inventory",
                                     sidebarLayout(
                                       sidebarPanel(h3("Landcover Category"),
-                                                   checkboxGroupInput(inputId = "pick_landcover",
+                                                   checkboxGroupInput(inputId = "select_landcover",
                                                                       label = "Select one or more:",
                                                                       #choices = unique(inventory$land_class))
-                                                                      choices = list("Orchard" = 1, "Vineyard" = 2, "Row Crop" = 3, "Rangeland" = 4)),
+                                                                      choices = list("Orchard" = 1,
+                                                                                     "Vineyard" = 2,
+                                                                                     "Row Crop" = 3,
+                                                                                     "Rangeland" = 4)),
                                       ),
                                       mainPanel(h2("Land Cover, Carbon Stocks & Emissions in 2016"),
-                                                #plotOutput("ci_plot")
+                                                leafletOutput("ci_plot")
                                       )
                                     )),
                            
+                           # Second Tab
                            tabPanel("Working Lands in 2030",
                                     fluidRow(
                                       column(5,
                                              "not sure how",
-                                             radioButtons("radio", 
+                                             radioButtons("project_var", 
                                                           label = "Select Variable",
-                                                          choices = list("Acreage" = 1, "Carbon Stock" = 2, "N2O Emissions" = 3), 
-                                                          selected = 1)
+                                                          choices = list("Acreage"=1,
+                                                                         "Carbon Stock"=2,
+                                                                         "N2O Emissions"=3)
+                                             )
                                       ),
                                       column(5,
                                              "this tab will look"
                                       ),
                                       mainPanel(h2("Our linear regression estimates this is what the County's working lands will look like in 2030"),
-                                                #plotOutput("ci_plot")
+                                                plotOutput("projection_plot")
                                       )
                                     )),
                            
+                           # Third Tab
                            tabPanel("Carbon-Smart Management Practices",
                                     sidebarLayout(
                                       sidebarPanel("Carbon-Smart Management Practices",
-                                                   checkboxGroupInput(inputId = "pick_practice",
+                                                   checkboxGroupInput(inputId = "select_practice",
                                                                       label = h3("Choose a management practice to learn more:"),
-                                                                      choices = list("Composting" = 1, "Cover Cropping" = 2, "Restoration" = 3)
+                                                                      choices = list("Composting" = 1,
+                                                                                     "Cover Cropping" = 2,
+                                                                                     "Restoration" = 3)
                                                                       #choices = unique(practices$carbon)
                                                    ),
                                                    
-                                                   sliderInput("acres_slide", label = h3("Acres"), min = 0, 
+                                                   sliderInput("acres_slide", label = h3("Percent of 2030 Acreage"), min = 0, 
                                                                max = 100, value = 50)
                                       ),
                                       mainPanel("Management Practices - Carbon Storage & Emissions Impacts",
-                                                #plotOutput("impact_plot")
+                                                plotOutput("impact_plot")
                                       )
                                     )),
                            
+                           # Fourth Tab
                            tabPanel("Barriers to Implementation",
                                     sidebarLayout(
-                                      sidebarPanel(selectInput("select", label = h3("Select a barrier"),
-                                                               choices = list("Cost/Funding" = 1, "Regulatory/Permitting" = 2, "Education" = 3, "Time" = 4, "Not Interested" = 5),
-                                                               selected = 1)),
-                                      mainPanel("See what other land managers have said, and add your own comments")
-                                    ))
+                                      sidebarPanel(selectInput("select_barrier",
+                                                               label = h3("Select a barrier"),
+                                                               choices = list("Cost/Funding",
+                                                                              "Regulatory/Permitting",
+                                                                              "Education",
+                                                                              "Time",
+                                                                              "Other"),
+                                                               selected = "Cost/Funding")),
+                                      mainPanel("See what other land managers have said, and add your own comments",
+                                                br(),
+                                                br(),
+                                                "These comments were provided anonymously through a survey distributed in September 2020 to a network of agricultural stakeholders in the County.",
+                                                br(),
+                                                br(),
+                                                tableOutput("selected_barrier") # issue with some options not being valid
+                                      )
+                                    )),
+                           
+                           # Fifth Tab
+                           tabPanel("Carbon Counters",
+                                    mainPanel(h2("Meet the team"),
+                                              "Alicia Fennell",
+                                              br(),
+                                              "Gavi Keyles",
+                                              br(),
+                                              "Madi Oliver",
+                                              br(),
+                                              "Minnie Ringland",
+                                              br(),
+                                              "Michael Wells"
+                                    )
+                           )
                 )
 )
 
 
-server <- function(input, output) {}
+# Server Interface
+server <- function(input, output) {
+  
+  # inventory code
+  mycols <- c("blue","green","red","purple") # colors not working
+  
+  output$ci_plot <- renderLeaflet({
+    map <- tm_shape(ca_subset) +
+      tm_fill(col=mycols[input$select_landcover])
+    
+    tmap_leaflet(map)
+  })
+  
+  # projection code
+  output$projection_plot <- renderPlot({
+    ggplot(data = matrix) +
+      geom_col(aes(x=year, y = input$project_var, fill = year))
+  })
+  
+  # mgmt practices code
+  mgmt_reactive <- reactive ({
+    matrix_mgmt <- matrix %>% 
+      mutate("Composting" = c(0.5,0.5),
+             "Cover Cropping" = c(1.5, 1.5),
+             "Restoration" = c(5,5)) %>% 
+      mutate("new" = input$select_practice*Acreage*(input$acres_slide/100))
+  })
+  
+  output$impact_plot <- renderPlot({
+    ggplot(data = matrix_mgmt) +
+      geom_col(aes(x=year, y = new))
+  })
+  
+  # barriers code
+  output$selected_barrier <- renderTable({
+    barriers %>% 
+      filter(barrier == input$select_barrier)
+  })
+  
+  
+}
 
 shinyApp(ui = ui, server = server)
