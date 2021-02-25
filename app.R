c63d3b178767c6dd038aeee9e30579b3eab1a57e
aliciafennell <aliciafennell@ucsb.edu>
2021-02-25 13:48:46 -0800

    ugh, not much new. Added team photos, tried to add some of Gavi's line graph code (fail for now)

--- app.R
+++ app.R
@@ -10,7 +10,7 @@ library(tmaptools)
 library(mapview)
 
 
-### Set themes
+########## Set themes
 dark_theme <- bs_theme(
   bg = "#053d57",
   fg = "#FFFAF0",
@@ -37,7 +37,9 @@ white_back_theme <- bs_theme(
 
 ui <- fluidPage(theme = dark_theme,
                 
-                navbarPage("Carbon Counters: Evaluating the Climate Mitigation Potential of Santa Barbara County's Natural and Working Lands",
+               navbarPage("CARBON COUNTERS",
+   ## I think it looks a little cleaner with a short title or no title - thoughts? 
+   ### Carbon Counters: Evaluating the Climate Mitigation Potential of Santa Barbara County's Natural and Working Lands
                            
                            tabPanel("Home",
                                     titlePanel("Evaluating the Climate Mitigation Potential of Santa Barbara County's Natural and Working Lands"),
@@ -103,7 +105,7 @@ ui <- fluidPage(theme = dark_theme,
                                                                                "N2O Emissions"=3)),
                                                    ),
                                       mainPanel(h3("Our linear regression estimates this is what the County's working lands will look like in 2030"),
-                                                plotOutput("projection_plot")
+                                                plotOutput("projection_plot")  
                                       )
                                     )),
                            
@@ -157,14 +159,19 @@ ui <- fluidPage(theme = dark_theme,
                                     mainPanel(h2("Meet the team"),
                                               br(),
                                               h4("Alicia Fennell"),
+                                              img(src = "alicia.jpeg", height = 300),
                                               br(),
-                                             h4("Gavi Keyles"),
+                                              h4("Gavi Keyles"),
+                                              img(src = "gavi.jpg", height = 300),
                                               br(),
                                               h4("Madi Oliver"),
+                                              img(src = "madi.jpg", height = 300),
                                               br(),
                                               h4("Minnie Ringland"),
+                                              img(src = "minnie.JPG", height = 300),
                                               br(),
-                                              h4("Michael Wells")
+                                              h4("Michael Wells"),
+                                              img(src = "michael.PNG", height = 300)
                                               )
                                     )
                            )
@@ -195,15 +202,116 @@ server <- function(input, output) {
   projection <- read_csv(here("data","projections.csv"))
   
   proj_reactive <- reactive ({
-    projection %>% 
+    projection %>%
       filter(type == input$project_var)
   })
-  
+
   output$projection_plot <- renderPlot({
     ggplot(data = proj_reactive()) +
       geom_point(aes(x = year, y = val, color = year),size = 3)
   })
   
+  ########### Gavi's code for line graphs  
+#   
+#   ag_observations <- read_csv(here("results", "all_ag_observations.csv"))
+#   ag_predictions <- read_csv(here("results", "all_predictions.csv")) 
+#   af_colors <- c("lightsteelblue", "goldenrod", "lightslategrey", "darkred", "sandybrown", "green4", "purple4")
+#   
+#   plot_predict <- ag_predictions %>% 
+#     mutate(land_class = str_to_title(land_class)) %>% 
+#     mutate(land_class = ifelse(land_class == "Row", "Row Crop", land_class)) %>% 
+#     filter(variable != "net") %>% 
+#     mutate(variable = factor(variable, levels = c("acres", "abvgc", "soilc", "noemit"))) %>% 
+#     mutate(land_class = ifelse(land_class == "Total_allclasses", "Total (all classes)", land_class))
+#   
+#   plot_observes <- ag_observations %>% 
+#     mutate(land_class = str_to_title(land_class)) %>% filter(variable != "net") %>% 
+#     mutate(variable = factor(variable, levels = c("acres", "abvgc", "soilc", "noemit"))) %>% 
+#     mutate(land_class = ifelse(land_class == "Total_allclasses", "Total (all classes)", land_class))
+#   
+#   inventory_16 <- read_csv(here("results", "inventory_16.csv")) %>% 
+#     clean_names()
+#   
+#   inventory_stocks <- inventory_16 %>% 
+#     mutate(total_stocks = total_soil_carbon_mtco2e + total_aboveground_carbon_mt_co2e) 
+#   
+#   tstock_plot_predict <- ag_predictions %>% 
+#     mutate(land_class = str_to_title(land_class)) %>% 
+#     mutate(land_class = ifelse(land_class == "Row", "Row Crop", land_class)) %>% 
+#     filter(variable != "net") %>% 
+#     mutate(variable = as.character(variable)) %>% 
+#     mutate(land_class = ifelse(land_class == "Total_allclasses", "Total (all classes)", land_class)) %>% 
+#     filter(variable %in% c("soilc", "abvgc")) %>% 
+#     group_by(year, land_class) %>% 
+#     summarize(value = sum(value)) %>% 
+#     mutate(variable = "total_stock") %>% 
+#     dplyr::select(c(1, 4, 2, 3))
+#   
+#   plot_predict <- data.frame(plot_predict)
+#   tstock_plot_predict <- data.frame(tstock_plot_predict)
+#   
+#   rev_plot_predict <- rbind(plot_predict, tstock_plot_predict)
+#   
+#   rev_plot_predict <- as.data.frame(rev_plot_predict)%>% 
+#     dplyr::mutate(variable = as.character(variable))
+#   
+#   tstock_plot_observes <- ag_observations %>% 
+#     mutate(land_class = str_to_title(land_class)) %>% filter(variable != "net") %>% 
+#     mutate(variable = as.character(variable)) %>% 
+#     mutate(land_class = ifelse(land_class == "Total_allclasses", "Total (all classes)", land_class)) %>% 
+#     filter(variable %in% c("soilc", "abvgc")) %>% 
+#     group_by(year, land_class) %>% 
+#     summarize(value = sum(value)) %>% 
+#     mutate(variable = "total_stock") %>% 
+#     dplyr::select(c(1, 4, 2, 3))
+#   
+#   plot_observes <- data.frame(plot_observes)
+#   tstock_plot_observes <- data.frame(tstock_plot_observes)
+#   
+#   rev_plot_observes <- rbind(plot_observes, tstock_plot_observes) %>% 
+#     mutate(year = as.numeric(year)) %>% 
+#     mutate(variable = as.character(variable)) %>% 
+#     mutate(value = as.numeric(value)) %>% 
+#     mutate(land_class = as.character(land_class))
+#   
+#   tstock_plot_all <- rev_plot_predict %>% 
+#     filter(year == "2030") %>% 
+#     mutate(year = as.numeric(year)) %>% 
+#     mutate(variable = as.character(variable)) %>% 
+#     mutate(value = as.numeric(value)) %>% 
+#     mutate(land_class = as.character(land_class))
+#   
+#   tstock_plot_all <- rbind(tstock_plot_all, rev_plot_observes)
+#   
+#   tstock_plot_acres <- tstock_plot_all %>% 
+#     filter(variable == "acres")
+#   
+#   tstock_plot_stock <- tstock_plot_all %>% 
+#     filter(variable == "total_stock")
+#   
+#   tstock_plot_n2o <- tstock_plot_all %>% 
+#     filter(variable == "noemit")
+#   
+#   labels <- unique(tstock_plot_acres$land_class)
+#   
+#   rev_plot_predict <- rev_plot_predict %>% 
+#     mutate(year = as.numeric(year))
+#   
+#   #### PLOTS 
+#   
+# updt_plot_acres_allclass <- ggplot() +
+#     geom_line(data = subset(rev_plot_predict, variable == "acres"), aes(x = year, y = value, group = land_class, color = land_class), size = .8, linetype = "dashed") +
+#     geom_point(data = tstock_plot_acres, aes(x = year, y = value, group = land_class, color = land_class), size = 3.6) +
+#     theme_minimal() +
+#     scale_color_manual(values = c("lightsteelblue", "goldenrod", "lightslategrey", "darkred", "sandybrown", "darkolivegreen3", "green4", "cornflowerblue", "purple4")) +
+#     labs(color = "Land Class",
+#          y = "Acres",
+#          x = "",
+#          title = "Estimated Past and Projected Future Acres by Land Class") +
+#     scale_y_continuous(labels = scales::comma) +
+#     scale_x_continuous(breaks = c(2012, 2016, 2019, 2030), labels = c("'12", "'16", "'19", "'30"))
+#   
+  ############
   
   ## mgmt practices code
   mgmt_reactive <- reactive ({
